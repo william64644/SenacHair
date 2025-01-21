66a2480fc94d06419a7a3f3d55906c85cb686a1a
william.taraujo <william.taraujo@TIT0628219W11-1>
2025-01-21 20:26:52 -0300

    adicionar formulario login

---
+++ SenacHair/frmLogin.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace SenacHair
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpLogin = new System.Windows.Forms.GroupBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.pctImagem = new System.Windows.Forms.PictureBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.grpLogin.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctImagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grpLogin
+            // 
+            this.grpLogin.Controls.Add(this.txtSenha);
+            this.grpLogin.Controls.Add(this.txtUsuario);
+            this.grpLogin.Controls.Add(this.lblSenha);
+            this.grpLogin.Controls.Add(this.lblUsuario);
+            this.grpLogin.Location = new System.Drawing.Point(109, 12);
+            this.grpLogin.Name = "grpLogin";
+            this.grpLogin.Size = new System.Drawing.Size(207, 84);
+            this.grpLogin.TabIndex = 9;
+            this.grpLogin.TabStop = false;
+            this.grpLogin.Text = "Dados para acesso";
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(60, 49);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(135, 20);
+            this.txtSenha.TabIndex = 3;
+            this.txtSenha.UseSystemPasswordChar = true;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(60, 23);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(135, 20);
+            this.txtUsuario.TabIndex = 2;
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Location = new System.Drawing.Point(11, 52);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(38, 13);
+            this.lblSenha.TabIndex = 1;
+            this.lblSenha.Text = "Senha";
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Location = new System.Drawing.Point(11, 26);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(43, 13);
+            this.lblUsuario.TabIndex = 0;
+            this.lblUsuario.Text = "Usuário";
+            // 
+            // pctImagem
+            // 
+            this.pctImagem.Location = new System.Drawing.Point(12, 12);
+            this.pctImagem.Name = "pctImagem";
+            this.pctImagem.Size = new System.Drawing.Size(91, 134);
+            this.pctImagem.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pctImagem.TabIndex = 8;
+            this.pctImagem.TabStop = false;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(214, 102);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(102, 44);
+            this.btnOk.TabIndex = 11;
+            this.btnOk.Text = "&Ok";
+            this.btnOk.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnOk.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnOk.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancelar.Location = new System.Drawing.Point(109, 102);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(102, 44);
+            this.btnCancelar.TabIndex = 10;
+            this.btnCancelar.Text = "&Cancelar";
+            this.btnCancelar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCancelar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 165);
+            this.Controls.Add(this.grpLogin);
+            this.Controls.Add(this.pctImagem);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.btnCancelar);
+            this.Name = "frmLogin";
+            this.Text = "frmLogin";
+            this.grpLogin.ResumeLayout(false);
+            this.grpLogin.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctImagem)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpLogin;
+        public System.Windows.Forms.TextBox txtSenha;
+        public System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.PictureBox pctImagem;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}

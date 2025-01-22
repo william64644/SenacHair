9fae570c2e03d936a98c28f8a4c4b5734659e2c7
VINICIO ROCHA DOS REIS <vinicio.rreis@senacsp.edu.br>
2025-01-21 21:43:28 -0300

    adicionar frmCliente

---
+++ SenacHair/frmCliente.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace SenacHair
+{
+    partial class frmCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPesquisa = new System.Windows.Forms.TextBox();
+            this.txtCarteira = new System.Windows.Forms.TextBox();
+            this.txtConvenio = new System.Windows.Forms.TextBox();
+            this.lblCelular = new System.Windows.Forms.Label();
+            this.txtCelular = new System.Windows.Forms.TextBox();
+            this.cboTipoCliente = new System.Windows.Forms.ComboBox();
+            this.txtCPF = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.lblPesquisa = new System.Windows.Forms.Label();
+            this.grpPesquisa = new System.Windows.Forms.GroupBox();
+            this.grpConvenio = new System.Windows.Forms.GroupBox();
+            this.lblCarteira = new System.Windows.Forms.Label();
+            this.lblConvenio = new System.Windows.Forms.Label();
+            this.lblTipoCliente = new System.Windows.Forms.Label();
+            this.lblCPF = new System.Windows.Forms.Label();
+            this.grpLista = new System.Windows.Forms.GroupBox();
+            this.grdDados = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.grpDados = new System.Windows.Forms.GroupBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnGravar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.grpPesquisa.SuspendLayout();
+            this.grpConvenio.SuspendLayout();
+            this.grpLista.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdDados)).BeginInit();
+            this.grpDados.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtPesquisa
+            // 
+            this.txtPesquisa.Location = new System.Drawing.Point(83, 22);
+            this.txtPesquisa.Name = "txtPesquisa";
+            this.txtPesquisa.Size = new System.Drawing.Size(265, 20);
+            this.txtPesquisa.TabIndex = 0;
+            // 
+            // txtCarteira
+            // 
+            this.txtCarteira.Location = new System.Drawing.Point(99, 46);
+            this.txtCarteira.MaxLength = 100;
+            this.txtCarteira.Name = "txtCarteira";
+            this.txtCarteira.Size = new System.Drawing.Size(216, 20);
+            this.txtCarteira.TabIndex = 1;
+            // 
+            // txtConvenio
+            // 
+            this.txtConvenio.Location = new System.Drawing.Point(99, 20);
+            this.txtConvenio.MaxLength = 100;
+            this.txtConvenio.Name = "txtConvenio";
+            this.txtConvenio.Size = new System.Drawing.Size(216, 20);
+            this.txtConvenio.TabIndex = 0;
+            // 
+            // lblCelular
+            // 
+            this.lblCelular.AutoSize = true;
+            this.lblCelular.Location = new System.Drawing.Point(169, 49);
+            this.lblCelular.Name = "lblCelular";
+            this.lblCelular.Size = new System.Drawing.Size(39, 13);
+            this.lblCelular.TabIndex = 25;
+            this.lblCelular.Text = "Celular";
+            // 
+            // txtCelular
+            // 
+            this.txtCelular.Location = new System.Drawing.Point(214, 47);
+            this.txtCelular.MaxLength = 100;
+            this.txtCelular.Name = "txtCelular";
+            this.txtCelular.Size = new System.Drawing.Size(134, 20);
+            this.txtCelular.TabIndex = 2;
+            // 
+            // cboTipoCliente
+            // 
+            this.cboTipoCliente.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboTipoCliente.FormattingEnabled = true;
+            this.cboTipoCliente.Location = new System.Drawing.Point(94, 73);
+            this.cboTipoCliente.Name = "cboTipoCliente";
+            this.cboTipoCliente.Size = new System.Drawing.Size(254, 21);
+            this.cboTipoCliente.TabIndex = 3;
+            // 
+            // txtCPF
+            // 
+            this.txtCPF.Location = new System.Drawing.Point(55, 47);
+            this.txtCPF.MaxLength = 100;
+            this.txtCPF.Name = "txtCPF";
+            this.txtCPF.Size = new System.Drawing.Size(113, 20);
+            this.txtCPF.TabIndex = 1;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(55, 21);
+            this.txtNome.MaxLength = 100;
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(293, 20);
+            this.txtNome.TabIndex = 0;
+            // 
+            // lblPesquisa
+            // 
+            this.lblPesquisa.AutoSize = true;
+            this.lblPesquisa.Location = new System.Drawing.Point(6, 25);
+            this.lblPesquisa.Name = "lblPesquisa";
+            this.lblPesquisa.Size = new System.Drawing.Size(71, 13);
+            this.lblPesquisa.TabIndex = 0;
+            this.lblPesquisa.Text = "Pesquisar por";
+            // 
+            // grpPesquisa
+            // 
+            this.grpPesquisa.Controls.Add(this.txtPesquisa);
+            this.grpPesquisa.Controls.Add(this.lblPesquisa);
+            this.grpPesquisa.Location = new System.Drawing.Point(12, 12);
+            this.grpPesquisa.Name = "grpPesquisa";
+            this.grpPesquisa.Size = new System.Drawing.Size(360, 53);
+            this.grpPesquisa.TabIndex = 3;
+            this.grpPesquisa.TabStop = false;
+            this.grpPesquisa.Text = "Dados para Pesquisa";
+            // 
+            // grpConvenio
+            // 
+            this.grpConvenio.Controls.Add(this.lblCarteira);
+            this.grpConvenio.Controls.Add(this.txtCarteira);
+            this.grpConvenio.Controls.Add(this.lblConvenio);
+            this.grpConvenio.Controls.Add(this.txtConvenio);
+            this.grpConvenio.Enabled = false;
+            this.grpConvenio.Location = new System.Drawing.Point(13, 96);
+            this.grpConvenio.Margin = new System.Windows.Forms.Padding(2);
+            this.grpConvenio.Name = "grpConvenio";
+            this.grpConvenio.Padding = new System.Windows.Forms.Padding(2);
+            this.grpConvenio.Size = new System.Drawing.Size(335, 77);
+            this.grpConvenio.TabIndex = 4;
+            this.grpConvenio.TabStop = false;
+            this.grpConvenio.Text = "Dados do Convênio";
+            // 
+            // lblCarteira
+            // 
+            this.lblCarteira.AutoSize = true;
+            this.lblCarteira.Location = new System.Drawing.Point(22, 48);
+            this.lblCarteira.Name = "lblCarteira";
+            this.lblCarteira.Size = new System.Drawing.Size(72, 13);
+            this.lblCarteira.TabIndex = 26;
+            this.lblCarteira.Text = "Nº Carteirinha";
+            // 
+            // lblConvenio
+            // 
+            this.lblConvenio.AutoSize = true;
+            this.lblConvenio.Location = new System.Drawing.Point(22, 22);
+            this.lblConvenio.Name = "lblConvenio";
+            this.lblConvenio.Size = new System.Drawing.Size(52, 13);
+            this.lblConvenio.TabIndex = 24;
+            this.lblConvenio.Text = "Convênio";
+            // 
+            // lblTipoCliente
+            // 
+            this.lblTipoCliente.AutoSize = true;
+            this.lblTipoCliente.Location = new System.Drawing.Point(10, 76);
+            this.lblTipoCliente.Name = "lblTipoCliente";
+            this.lblTipoCliente.Size = new System.Drawing.Size(78, 13);
+            this.lblTipoCliente.TabIndex = 21;
+            this.lblTipoCliente.Text = "Tipo de Cliente";
+            // 
+            // lblCPF
+            // 
+            this.lblCPF.AutoSize = true;
+            this.lblCPF.Location = new System.Drawing.Point(10, 49);
+            this.lblCPF.Name = "lblCPF";
+            this.lblCPF.Size = new System.Drawing.Size(27, 13);
+            this.lblCPF.TabIndex = 22;
+            this.lblCPF.Text = "CPF";
+            // 
+            // grpLista
+            // 
+            this.grpLista.Controls.Add(this.grdDados);
+            this.grpLista.Location = new System.Drawing.Point(12, 71);
+            this.grpLista.Name = "grpLista";
+            this.grpLista.Size = new System.Drawing.Size(360, 215);
+            this.grpLista.TabIndex = 4;
+            this.grpLista.TabStop = false;
+            this.grpLista.Text = "Clientes Cadastrados";
+            // 
+            // grdDados
+            // 
+            this.grdDados.AllowUserToAddRows = false;
+            this.grdDados.AllowUserToDeleteRows = false;
+            this.grdDados.AllowUserToResizeColumns = false;
+            this.grdDados.AllowUserToResizeRows = false;
+            this.grdDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdDados.Location = new System.Drawing.Point(6, 19);
+            this.grdDados.MultiSelect = false;
+            this.grdDados.Name = "grdDados";
+            this.grdDados.ReadOnly = true;
+            this.grdDados.RowHeadersVisible = false;
+            this.grdDados.RowHeadersWidth = 62;
+            this.grdDados.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.grdDados.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grdDados.Size = new System.Drawing.Size(342, 190);
+            this.grdDados.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Nome";
+            // 
+            // grpDados
+            // 
+            this.grpDados.Controls.Add(this.lblCelular);
+            this.grpDados.Controls.Add(this.txtCelular);
+            this.grpDados.Controls.Add(this.grpConvenio);
+            this.grpDados.Controls.Add(this.cboTipoCliente);
+            this.grpDados.Controls.Add(this.lblTipoCliente);
+            this.grpDados.Controls.Add(this.lblCPF);
+            this.grpDados.Controls.Add(this.txtCPF);
+            this.grpDados.Controls.Add(this.label5);
+            this.grpDados.Controls.Add(this.txtNome);
+            this.grpDados.Location = new System.Drawing.Point(12, 292);
+            this.grpDados.Name = "grpDados";
+            this.grpDados.Size = new System.Drawing.Size(360, 185);
+            this.grpDados.TabIndex = 5;
+            this.grpDados.TabStop = false;
+            this.grpDados.Text = "Dados para Cadastro";
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Image = global::SenacHair.Properties.Resources.eraser2;
+            this.btnLimpar.Location = new System.Drawing.Point(133, 483);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(115, 41);
+            this.btnLimpar.TabIndex = 25;
+            this.btnLimpar.Text = "&Limpar";
+            this.btnLimpar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnLimpar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            // 
+            // btnGravar
+            // 
+            this.btnGravar.Image = global::SenacHair.Properties.Resources.floppy_disks;
+            this.btnGravar.Location = new System.Drawing.Point(12, 483);
+            this.btnGravar.Name = "btnGravar";
+            this.btnGravar.Size = new System.Drawing.Size(115, 41);
+            this.btnGravar.TabIndex = 24;
+            this.btnGravar.Text = "&Gravar";
+            this.btnGravar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnGravar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnGravar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Image = global::SenacHair.Properties.Resources.delete;
+            this.btnCancelar.Location = new System.Drawing.Point(256, 483);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(115, 41);
+            this.btnCancelar.TabIndex = 26;
+            this.btnCancelar.Text = "&Cancelar";
+            this.btnCancelar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCancelar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // frmCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 551);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnGravar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.grpPesquisa);
+            this.Controls.Add(this.grpLista);
+            this.Controls.Add(this.grpDados);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmCliente";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Senac Cadastro de Clientes";
+            this.grpPesquisa.ResumeLayout(false);
+            this.grpPesquisa.PerformLayout();
+            this.grpConvenio.ResumeLayout(false);
+            this.grpConvenio.PerformLayout();
+            this.grpLista.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grdDados)).EndInit();
+            this.grpDados.ResumeLayout(false);
+            this.grpDados.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPesquisa;
+        private System.Windows.Forms.TextBox txtCarteira;
+        private System.Windows.Forms.TextBox txtConvenio;
+        private System.Windows.Forms.Label lblCelular;
+        private System.Windows.Forms.TextBox txtCelular;
+        private System.Windows.Forms.ComboBox cboTipoCliente;
+        private System.Windows.Forms.TextBox txtCPF;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label lblPesquisa;
+        private System.Windows.Forms.GroupBox grpPesquisa;
+        private System.Windows.Forms.GroupBox grpConvenio;
+        private System.Windows.Forms.Label lblCarteira;
+        private System.Windows.Forms.Label lblConvenio;
+        private System.Windows.Forms.Label lblTipoCliente;
+        private System.Windows.Forms.Label lblCPF;
+        private System.Windows.Forms.GroupBox grpLista;
+        private System.Windows.Forms.DataGridView grdDados;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox grpDados;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnGravar;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}

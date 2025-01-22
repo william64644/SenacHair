ebb79766858ce4558bfcbb1936ad468c78585428
WAGNER ARAUJO CORREIA <wagner.acorreia@senacsp.edu.br>
2025-01-21 21:41:40 -0300

    adicionar frmCabelereiro

---
+++ SenacHair/frmCabeleireiro.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace HappySmile
+{
+    partial class frmCabeleireiro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCabeleireiro));
+            this.grpDados = new System.Windows.Forms.GroupBox();
+            this.btnRem = new System.Windows.Forms.Button();
+            this.lstEspecialidades = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.cboEspecialidades = new System.Windows.Forms.ComboBox();
+            this.lblEspecialidades = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.grpLista = new System.Windows.Forms.GroupBox();
+            this.grdDados = new System.Windows.Forms.DataGridView();
+            this.grpPesquisa = new System.Windows.Forms.GroupBox();
+            this.txtPesquisa = new System.Windows.Forms.TextBox();
+            this.lblPesquisa = new System.Windows.Forms.Label();
+            this.btnGravar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.grpDados.SuspendLayout();
+            this.grpLista.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdDados)).BeginInit();
+            this.grpPesquisa.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpDados
+            // 
+            this.grpDados.Controls.Add(this.btnRem);
+            this.grpDados.Controls.Add(this.lstEspecialidades);
+            this.grpDados.Controls.Add(this.btnAdd);
+            this.grpDados.Controls.Add(this.cboEspecialidades);
+            this.grpDados.Controls.Add(this.lblEspecialidades);
+            this.grpDados.Controls.Add(this.label1);
+            this.grpDados.Controls.Add(this.txtEmail);
+            this.grpDados.Controls.Add(this.label5);
+            this.grpDados.Controls.Add(this.txtNome);
+            this.grpDados.Location = new System.Drawing.Point(12, 292);
+            this.grpDados.Name = "grpDados";
+            this.grpDados.Size = new System.Drawing.Size(360, 214);
+            this.grpDados.TabIndex = 14;
+            this.grpDados.TabStop = false;
+            this.grpDados.Text = "Dados para Cadastro";
+            // 
+            // btnRem
+            // 
+            this.btnRem.Location = new System.Drawing.Point(323, 102);
+            this.btnRem.Name = "btnRem";
+            this.btnRem.Size = new System.Drawing.Size(25, 27);
+            this.btnRem.TabIndex = 25;
+            this.btnRem.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnRem.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnRem.UseVisualStyleBackColor = true;
+            this.btnRem.Click += new System.EventHandler(this.btnRem_Click);
+            // 
+            // lstEspecialidades
+            // 
+            this.lstEspecialidades.FormattingEnabled = true;
+            this.lstEspecialidades.Location = new System.Drawing.Point(94, 100);
+            this.lstEspecialidades.Name = "lstEspecialidades";
+            this.lstEspecialidades.Size = new System.Drawing.Size(223, 108);
+            this.lstEspecialidades.TabIndex = 24;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(323, 69);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(25, 27);
+            this.btnAdd.TabIndex = 23;
+            this.btnAdd.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnAdd.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // cboEspecialidades
+            // 
+            this.cboEspecialidades.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboEspecialidades.FormattingEnabled = true;
+            this.cboEspecialidades.Location = new System.Drawing.Point(94, 73);
+            this.cboEspecialidades.Name = "cboEspecialidades";
+            this.cboEspecialidades.Size = new System.Drawing.Size(223, 21);
+            this.cboEspecialidades.TabIndex = 22;
+            // 
+            // lblEspecialidades
+            // 
+            this.lblEspecialidades.AutoSize = true;
+            this.lblEspecialidades.Location = new System.Drawing.Point(10, 76);
+            this.lblEspecialidades.Name = "lblEspecialidades";
+            this.lblEspecialidades.Size = new System.Drawing.Size(78, 13);
+            this.lblEspecialidades.TabIndex = 21;
+            this.lblEspecialidades.Text = "Especialidades";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(55, 47);
+            this.txtEmail.MaxLength = 100;
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(293, 20);
+            this.txtEmail.TabIndex = 21;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Nome";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(55, 21);
+            this.txtNome.MaxLength = 100;
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(293, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // grpLista
+            // 
+            this.grpLista.Controls.Add(this.grdDados);
+            this.grpLista.Location = new System.Drawing.Point(12, 71);
+            this.grpLista.Name = "grpLista";
+            this.grpLista.Size = new System.Drawing.Size(360, 215);
+            this.grpLista.TabIndex = 13;
+            this.grpLista.TabStop = false;
+            this.grpLista.Text = "Dentistas Cadastrados";
+            // 
+            // grdDados
+            // 
+            this.grdDados.AllowUserToAddRows = false;
+            this.grdDados.AllowUserToDeleteRows = false;
+            this.grdDados.AllowUserToResizeColumns = false;
+            this.grdDados.AllowUserToResizeRows = false;
+            this.grdDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdDados.Location = new System.Drawing.Point(6, 19);
+            this.grdDados.MultiSelect = false;
+            this.grdDados.Name = "grdDados";
+            this.grdDados.ReadOnly = true;
+            this.grdDados.RowHeadersVisible = false;
+            this.grdDados.RowHeadersWidth = 62;
+            this.grdDados.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grdDados.Size = new System.Drawing.Size(342, 190);
+            this.grdDados.TabIndex = 0;
+            this.grdDados.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.grdDados_CellClick);
+            // 
+            // grpPesquisa
+            // 
+            this.grpPesquisa.Controls.Add(this.txtPesquisa);
+            this.grpPesquisa.Controls.Add(this.lblPesquisa);
+            this.grpPesquisa.Location = new System.Drawing.Point(12, 12);
+            this.grpPesquisa.Name = "grpPesquisa";
+            this.grpPesquisa.Size = new System.Drawing.Size(360, 53);
+            this.grpPesquisa.TabIndex = 12;
+            this.grpPesquisa.TabStop = false;
+            this.grpPesquisa.Text = "Dados para Pesquisa";
+            // 
+            // txtPesquisa
+            // 
+            this.txtPesquisa.Location = new System.Drawing.Point(83, 22);
+            this.txtPesquisa.Name = "txtPesquisa";
+            this.txtPesquisa.Size = new System.Drawing.Size(265, 20);
+            this.txtPesquisa.TabIndex = 0;
+            this.txtPesquisa.TextChanged += new System.EventHandler(this.txtPesquisa_TextChanged);
+            // 
+            // lblPesquisa
+            // 
+            this.lblPesquisa.AutoSize = true;
+            this.lblPesquisa.Location = new System.Drawing.Point(6, 25);
+            this.lblPesquisa.Name = "lblPesquisa";
+            this.lblPesquisa.Size = new System.Drawing.Size(71, 13);
+            this.lblPesquisa.TabIndex = 0;
+            this.lblPesquisa.Text = "Pesquisar por";
+            // 
+            // btnGravar
+            // 
+            this.btnGravar.Location = new System.Drawing.Point(13, 512);
+            this.btnGravar.Name = "btnGravar";
+            this.btnGravar.Size = new System.Drawing.Size(115, 41);
+            this.btnGravar.TabIndex = 15;
+            this.btnGravar.Text = "&Gravar";
+            this.btnGravar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnGravar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnGravar.UseVisualStyleBackColor = true;
+            this.btnGravar.Click += new System.EventHandler(this.btnGravar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(257, 512);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(115, 41);
+            this.btnCancelar.TabIndex = 17;
+            this.btnCancelar.Text = "&Cancelar";
+            this.btnCancelar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCancelar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(135, 512);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(115, 41);
+            this.btnLimpar.TabIndex = 16;
+            this.btnLimpar.Text = "&Limpar";
+            this.btnLimpar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnLimpar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // frmDentista
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 558);
+            this.Controls.Add(this.btnGravar);
+            this.Controls.Add(this.grpDados);
+            this.Controls.Add(this.grpLista);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.grpPesquisa);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmDentista";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Senac - Happy Smile - Cadastro de Dentistas";
+            this.Load += new System.EventHandler(this.frmDentista_Load);
+            this.grpDados.ResumeLayout(false);
+            this.grpDados.PerformLayout();
+            this.grpLista.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grdDados)).EndInit();
+            this.grpPesquisa.ResumeLayout(false);
+            this.grpPesquisa.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGravar;
+        private System.Windows.Forms.GroupBox grpDados;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.GroupBox grpLista;
+        private System.Windows.Forms.DataGridView grdDados;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.GroupBox grpPesquisa;
+        private System.Windows.Forms.TextBox txtPesquisa;
+        private System.Windows.Forms.Label lblPesquisa;
+        private System.Windows.Forms.ComboBox cboEspecialidades;
+        private System.Windows.Forms.Label lblEspecialidades;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRem;
+        private System.Windows.Forms.ListBox lstEspecialidades;
+    }
+}
---
+++ SenacHair/frmCabeleireiro.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HappySmile
+{
+    public partial class frmCabeleireiro : Form
+    {
+        public frmCabeleireiro()
+        {
+            InitializeComponent();
+        }
+        //Instanciamos o objeto principal do formulário
+        Dentista dentista = new Dentista();
+        //Criamos o método para preencher o ComboBox com as especialidades disponíveis no banco de dados
+        private void PreencherEspecialidades()
+        {
+            //definimos um bloco try/catch para garantir a execução
+            //do fluxo fora do nosso ambiente
+            try
+            {
+                //Instanciamos o objeto
+                Especialidade e = new Especialidade();
+                //Carregamos o ComboBox com o resultado da consulta das especialidades (DataTable)
+                cboEspecialidades.DataSource = e.Consultar();
+                //Definimos o valor a ser exibido para o usuário
+                cboEspecialidades.DisplayMember = "Especialidade";
+                //Definimos o valor a ser armazenado após o usuário selecionar um item
+                cboEspecialidades.ValueMember = "Id";
+                //Definimos que o ComboBox irá ser iniciado sem nenhuma opção escolhida
+                cboEspecialidades.SelectedIndex = -1;
+            }
+            catch (Exception ex)
+            {
+                //Caso ocorra algum erro fora do ambiente
+                //Mostramos a mensagem de erro ao usuário
+                MessageBox.Show("Erro-->" + ex.Message, "Erro",
+                                   MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void frmDentista_Load(object sender, EventArgs e)
+        {
+            //Chamamos o método para preencher o ComboBox com as especialidades
+            PreencherEspecialidades();
+            //Copiamos as definições de exibição e atribuição de valores do ComboBox para o ListBox
+            lstEspecialidades.DisplayMember = cboEspecialidades.DisplayMember;
+            lstEspecialidades.ValueMember = cboEspecialidades.ValueMember;
+            //Chamamos o método para preencher o Grid com os Dentistas cadastrados
+            CarregarGrid();
+        }
+        private void CarregarGrid()
+        {
+            //definimos um bloco try/catch para garantir a execução
+            //do fluxo fora do nosso ambiente
+            try
+            {
+                //Preenchemos o Grid com o resultado da consulta dos dentistas
+                grdDados.DataSource = dentista.Consultar();
+                //Ocultar colunas
+                grdDados.Columns[0].Visible = false; //Id
+                //Cabeçalho das colunas
+                grdDados.Columns[1].HeaderText = "Nome";
+                grdDados.Columns[2].HeaderText = "Email";
+                //Largura das colunas
+                grdDados.Columns[1].Width = 250;
+                grdDados.Columns[2].Width = 150;
+
+            }
+            catch (Exception ex)
+            {
+                //Caso ocorra algum erro fora do ambiente
+                //Mostramos a mensagem de erro ao usuário
+                MessageBox.Show("Erro-->" + ex.Message, "Erro",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            //Caso não tenhamos nenhuma especialidade selecionada, não executamos nenhuma ação
+            if (cboEspecialidades.SelectedIndex == -1) return;            
+
+            if (lstEspecialidades.Items.IndexOf(cboEspecialidades.SelectedItem) != -1)
+            {
+                //Caso a especialidade já tenha sido adicionada anteriormente
+                //Exibimos a mensagem ao usuário e não executamos nenhuma ação
+                MessageBox.Show("Especialidade já selecionada.",
+                    "Cadastro de Dentistas",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            //Adicionamos ao ListBox a especialidade selecionada no ComboBox
+            lstEspecialidades.Items.Add(cboEspecialidades.SelectedItem);
+            //Posicionamos o ComboBox na opção em branco
+            cboEspecialidades.SelectedIndex = -1;
+        }
+        private void txtPesquisa_TextChanged(object sender, EventArgs e)
+        {
+            //Resetamos o objeto             
+            dentista = new Dentista();
+            //Atribuímos o nome para pesquisa
+            dentista.nome = txtPesquisa.Text;
+            //Chamamos o método para preencher o Grid com o resultado do filtro aplicado
+            CarregarGrid();
+        }
+        private void PreencherFormulario()
+        {
+            //Preenchemos os controle do formulário com suas
+            //respectivas informações contidas no objeto
+            txtNome.Text = dentista.nome;
+            txtEmail.Text = dentista.email;
+            //Limpamos o ListBox
+            lstEspecialidades.Items.Clear();
+            //Percorremos todas as especialidades retornadas do banco de dados
+            foreach (Especialidade e in dentista.especialidades)
+            {
+                //Posicionamos o ComboBox na opção correspondente à especialidade cadastrada
+                cboEspecialidades.SelectedValue = e.id;
+                //Simulamos o click no botão Adicionar (+)
+                btnAdd.PerformClick();
+            }
+        }
+        private void PreencherClasse()
+        {
+            //Atribuímos às propriedades do objeto os respectivos valores informados no formulário
+            dentista.nome = txtNome.Text;
+            dentista.email = txtEmail.Text;
+            //Limpamos a lista de especialidades
+            dentista.especialidades.Clear();
+            //Percorremos todas as especialidades adicionadas ao ListBox
+            foreach (DataRowView item in lstEspecialidades.Items)
+            {
+                //Instanciamos um objeto de especialidade
+                Especialidade e = new Especialidade();
+                //Atribuímos os respectivos valores às propriedades do objeto
+                e.id = Convert.ToInt32(item.Row.ItemArray[0]);
+                e.especialidade = item.Row.ItemArray[1].ToString();
+                //Adicionamos o objeto à lista de especialidades do objeto principal
+                dentista.especialidades.Add(e);
+            }
+        }
+        private void grdDados_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //definimos um bloco try/catch para garantir a execução
+            //do fluxo fora do nosso ambiente
+            try
+            {
+                //Resetamos o objeto  
+                dentista = new Dentista();
+                //Atribuímos o Id do dentista a propriedade
+                dentista.id = Convert.ToInt32(grdDados.SelectedRows[0].Cells[0].Value);
+                //Efetuamos a consulta para obter os dados do dentista
+                dentista.Consultar();
+                //Chamamos o método para preencher os dados do formulário
+                PreencherFormulario();
+            }
+            catch (Exception ex)
+            {
+                //Caso ocorra algum erro fora do ambiente
+                //Mostramos a mensagem de erro ao usuário
+                MessageBox.Show("Erro-->" + ex.Message, "Erro",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void btnGravar_Click(object sender, EventArgs e)
+        {
+            //definimos um bloco try/catch para garantir a execução
+            //do fluxo fora do nosso ambiente
+            try
+            {
+                //Efetuamos a validação do preenchimento do formulário
+                //e armazenamos as mensagens de erro retornadas pelo método
+                string mensagemErro = ValidarPreenchimento();
+                if (mensagemErro != string.Empty)
+                {
+                    //Caso seja encontrada algum conteúdo na mensagem de Erro
+                    //Exibimos a mensagem ao usuário e encerramos o fluxo de gravação
+                    MessageBox.Show(mensagemErro, "Erro de Preenchimento",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                //Chamamos o método para preencher o objeto com os dados
+                //Preenchidos no formulário
+                PreencherClasse();
+                //Chamamos o método para efetivar a gravação dos dados
+                dentista.Gravar();
+                //Exibimos a mensagem de sucesso na gravação dos dados
+                MessageBox.Show("Dentista gravado com sucesso!",
+                    "Cadastro de Dentistas",
+                   MessageBoxButtons.OK, MessageBoxIcon.Information);
+                //chamamos o método para limpar o formulário
+                LimparCampos();
+                //Atualizamos o grid com os dados gravados no banco de dados
+                CarregarGrid();
+            }
+            catch (Exception ex)
+            {
+                //Caso ocorra algum erro fora do ambiente
+                //Mostramos a mensagem de erro ao usuário
+                MessageBox.Show("Erro-->" + ex.Message, "Erro",
+                                   MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            //Fechamos o formulário
+            Close();
+        }
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            //Chamamos o método para limpar o formulário
+            LimparCampos();
+        }
+        private void LimparCampos()
+        {
+            //Resetamos o objeto 
+            dentista = new Dentista();
+            //Limpamos todos os TextBox
+            txtPesquisa.Clear();
+            txtNome.Clear();
+            txtEmail.Clear();
+            //Removemos todos os items adicionados no ListBox
+            lstEspecialidades.Items.Clear();
+            //Posicionamos o ComboBox na opção em branco
+            cboEspecialidades.SelectedIndex = -1;
+            //colocamos o cursor no TextBox de Pesquisa
+            txtPesquisa.Focus();
+        }
+        private string ValidarPreenchimento()
+        {
+            //Fazemos a validação do preenchimento do formulário
+            string msgErro = string.Empty;
+            if (txtNome.Text == string.Empty)
+            {
+                msgErro = "Preencha o NOME.\n";
+            }
+            if (txtEmail.Text == string.Empty)
+            {
+                msgErro += "Preencha o EMAIL.\n";
+            }
+            if (lstEspecialidades.Items.Count == 0)
+            {
+                msgErro += "Preencha as ESPECIALIDADES.\n";
+            }
+            //Retornamos as mensagens de erro encontradas
+            return msgErro;
+        }
+        private void btnRem_Click(object sender, EventArgs e)
+        {
+            //Caso não tenhamos nenhuma especialidade adicionada, não executamos nenhuma ação
+            if (lstEspecialidades.SelectedIndex == -1) return;
+
+            //Exibimos a mensagem ao usuário, solicitando a confirmação da exclusão
+            //da especialidade selecionada
+            string item = lstEspecialidades.Text;
+            if (MessageBox.Show($"Deseja remover o item {item}?",
+                "Cadastro de Dentistas",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question,
+                MessageBoxDefaultButton.Button2) == DialogResult.No) return;
+            //Caso o usuário confirma a intenção, removemos a especialidade selecionada
+            lstEspecialidades.Items.Remove(lstEspecialidades.SelectedItem);
+        }
+    }
+}
